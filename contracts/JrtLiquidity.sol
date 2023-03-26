52b9c59aa721a0f077fd55b22bc7134a007444a2
Lamont V <lxveal1983@gmail.com>
2023-03-26 11:17:25 -0400

    Update JrtLiquidity.sol

--- contracts/JrtLiquidity.sol
+++ contracts/JrtLiquidity.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
+
 import ".deps/npm/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IJackrabbitV2Factory.sol";
 import "./interfaces/IJackrabbitV2Router02.sol";
+
 contract JrtLiquidity {
-  address private constant FACTORY = 0x5934873D064f6b544918C7481d71F4806742Be58;
-  address private constant ROUTER = 0xC6E38122bed2a2307982F7A8902136e8D5bbeaD7;
+  address private constant FACTORY = ;
+  address private constant ROUTER = ;
   address private constant WETH = ;
   address private constant FIL = ;
+  
   event Log(string message, uint val);
+  
   function addLiquidity(
     address _tokenA,
     address _tokenB,
@@ -30,10 +34,12 @@ contract JrtLiquidity {
         address(this),
         block.timestamp
       );
+      
     emit Log("amountA", amountA);
     emit Log("amountB", amountB);
     emit Log("liquidity", liquidity);
   }
+  
   function removeLiquidity(address _tokenA, address _tokenB) external {
     address pair = IJackrabbitV2Factory(FACTORY).getPair(_tokenA, _tokenB);
     uint liquidity = IERC20(pair).balanceOf(address(this));
@@ -48,6 +54,7 @@ contract JrtLiquidity {
         address(this),
         block.timestamp
       );
+      
     emit Log("amountA", amountA);
     emit Log("amountB", amountB);
   }
